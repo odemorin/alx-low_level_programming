34ad36affec245c8d6375bbf9f14ff65e2794197
odemorin <tundeadeolu20@gmail.com>
2022-09-14 08:37:07 -0700

    trial

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	for(int x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(tolower(x));
+	}
+	return (0);
+
+}
