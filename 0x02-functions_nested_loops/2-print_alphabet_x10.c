ebb6c85d96258161ba7af487d4d9d0aaf835a492
odemorin <tundeadeolu20@gmail.com>
2022-09-14 21:31:17 -0700

    trial

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,27 @@
  *
  */
 void print_alphabet_x10(void)
+
 {
-	char i,j;
-	for (i=o; i<=9; i++)
-	{
-		for (j ='a'; j<= 'z'; j++)
-		{
-			_putchar(j);
-		}
-		_putchar('\n');
-	}
+
+		char i, j;
+
+
+
+			for (i = 0; i <= 9; i++)
+
+					{
+
+								for (j = 'a'; j <= 'z'; j++)
+
+											{
+
+															_putchar(j);
+
+																	}
+
+										_putchar('\n');
+
+											}
+
 }
