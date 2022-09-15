2868d391f164fcd35970b5cca7c6660ff30fa4f3
odemorin <tundeadeolu20@gmail.com>
2022-09-14 21:20:08 -0700

    trial

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabeth_x10 - print abc 10
+ *
+ * Retuen: 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char i,j;
+	for (i=o; i<=9; i++)
+	{
+		for (j ='a'; j<= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_last_digit - print last digit of a num
+ *
+ *   *
+ *
+ *    * @n: integer to be used
+ *
+ *     *
+ *
+ *      * Return: last digit of int
+ *
+ *       *
+ *
+ *        */
+
+int print_last_digit(int n)
+
+{
+
+		int last_digit = n % 10;
+
+
+
+			if (last_digit < 0)
+
+						last_digit *= -1;
+
+
+
+				_putchar(last_digit + '0');
+
+
+
+					return (last_digit);
+
+}
