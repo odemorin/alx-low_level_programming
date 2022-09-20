2212cf9a16f74af7971959242b8be8e13efce210
odemorin <tundeadeolu20@gmail.com>
2022-09-20 08:22:16 -0700

    t

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,16 +3,13 @@
 
 
 /**
+ * reset_to_98 - resets value of pointer to 98
  *
- *  * reset_to_98 - resets value of pointer to 98
+ * @n: pointer to reset to 98
  *
- *   * @n: pointer to reset to 98
+ * Return: void
  *
- *    *
- *
- *     * Return: void
- *
- *      */
+ */
 
 void reset_to_98(int *n)
 
