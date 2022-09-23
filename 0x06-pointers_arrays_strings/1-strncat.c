ef51b7fed6b238bcce9e67b9b17e5d8a60d6bb2e
odemorin <tundeadeolu20@gmail.com>
2022-09-23 02:40:48 -0700

    t

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,10 +13,11 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
-	
+
 	i = j = 0;
+
 	while (*(dest + i))
-		i++;
+	i++;
 	while (j < n && *(src + j))
 	{
 		*(dest + i) = *(src + j);
