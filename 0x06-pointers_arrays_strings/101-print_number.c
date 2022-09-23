c6ded552338d10abdd89a9f3a48201c8fb44f385
odemorin <tundeadeolu20@gmail.com>
2022-09-23 05:02:06 -0700

    t

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * print_number - takes an integer and prints it with _putchar
+ * print_number - takes an integer and prints it with _putchar
  *   * @n: integer to print
  *    *
  *     * Return: void
- *      */
+ */
 void print_number(int n)
 {
 		int a0, a1, a2, a3, a4, a5, a6, a7, a8, a9;
