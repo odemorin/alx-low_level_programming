1cd9cf3fe237751f9eba6363a380aac582979687
odemorin <tundeadeolu20@gmail.com>
2022-09-23 07:08:42 -0700

    t

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,7 +3,6 @@
 
 
 /**
- *
  *  * leet - encodes a string into 1337
  *
  *   * @s: string to encode
@@ -12,36 +11,21 @@
  *
  *     * Return: address of s
  *
- *      */
+ */
 
 char *leet(char *s)
-
 {
-
-		int i, j;
-
-			char a[] = "aAeEoOtTlL";
-
-				char b[] = "4433007711";
-
-
-
-					for (i = 0; *(s + i); i++)
-
-							{
-
-										for (j = 0; j <= 9; j++)
-
-													{
-
-																	if (a[j] == *(s + i))
-
-																						*(s + i) = b[j];
-
-																			}
-
-											}
-
-						return (s);
-
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+	
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
+		}
+	}
+return (s);
 }
